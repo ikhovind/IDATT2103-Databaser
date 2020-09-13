c3bfd9f5074d9da573a9c95db46af236accaf45f
Ingebrigt <ing.hovind@hotmail.com>
2020-09-13 21:54:28 +0200

    finished 2

--- Oevinger/Oeving 2/Del1/Oppgave B.sql
+++ Oevinger/Oeving 2/Del1/Oppgave B.sql
@@ -1,2 +1,2 @@
 SELECT * FROM bok CROSS join forlag
-#Hver bok blir satt i en tuppel med alle mulige forlagene. Det er 8 forskjellige forlag, så man kan se at hver bok repeteres 8 ganger før man øker til neste
+#Hver bok blir satt i en tuppel med hver av forlagene. Det er 8 forskjellige forlag, så man kan se at hver bok repeteres 8 ganger
