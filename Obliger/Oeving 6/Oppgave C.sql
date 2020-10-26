379d9a6f683fb644ceb262366b75c25caee133fa
Ingebrigt <ing.hovind@hotmail.com>
2020-10-26 13:18:43 +0100

    ferdig med øving 6

--- Obliger/Oeving 6/Oppgave C.sql
+++ Obliger/Oeving 6/Oppgave C.sql
@@ -1,10 +1,12 @@
 USE ikhovind;
+SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS kvalifikasjon;
 DROP TABLE IF EXISTS kandidat;
 DROP TABLE IF EXISTS sluttatest_mal;
 DROP TABLE IF EXISTS bedrift;
 DROP TABLE IF EXISTS kandidat_kvalifikasjon;
-
+DROP TABLE IF EXISTS oppdrag;
+SET FOREIGN_KEY_CHECKS=1;
 CREATE TABLE kvalifikasjon(
  kvalifikasjon_id INTEGER NOT NULL AUTO_INCREMENT,
  beskrivelse VARCHAR(30),
@@ -36,11 +38,11 @@ CONSTRAINT org_pk PRIMARY KEY(orgnummer));
 CREATE TABLE oppdrag(
  oppdrag_id INTEGER NOT NULL AUTO_INCREMENT,
  startdato DATE,
- sluttdato DATE,
- ant_timer INTEGER DEFAULT 0,
+ sluttdato DATE default NULL,
  org_nummer INTEGER,
- kandidat_id INTEGER,
  kvalifikasjon_id INTEGER,
+ kandidat_id INTEGER,
+ ant_timer INTEGER DEFAULT 0,
 CONSTRAINT oppdrag_pk PRIMARY KEY(oppdrag_id),
 FOREIGN KEY (org_nummer) REFERENCES bedrift(orgnummer),
 FOREIGN KEY (kandidat_id) REFERENCES kandidat(kandidat_id),
@@ -56,3 +58,25 @@ FOREIGN KEY (kandidat_id) REFERENCES kandidat(kandidat_id),
 FOREIGN KEY (kvalifikasjon_id) REFERENCES kvalifikasjon(kvalifikasjon_id)
 );
 
+insert into kvalifikasjon values (1,'Truckførerbevis');
+insert into kvalifikasjon values (2, 'Førerkort klasse B');
+insert into kvalifikasjon values (3, 'Fagbrev');
+
+insert into kandidat values (1, 'Ingebrigt', 'Hovind', 123, '123@gmail.com');
+insert into kandidat values (2, 'Jonas', 'Thomassen', 1234, '1234@gmail.com');
+insert into kandidat values (3, 'Runar', 'Ness', 12345, '12345@gmail.com');
+
+insert into sluttatest_mal values (1, 'Vellykket arbeid :)');
+
+insert into bedrift values (1, 'Byggmakker', 5280, 'byggmakker@gmail.com');
+insert into bedrift values (2, 'Linde', 5290, 'Linde@gmail.com');
+insert into bedrift values (3, 'NTNU', 5300, 'NTNU@gmail.com');
+
+insert into oppdrag values (1, DATE('2020-03-12'), null, 3, NULL, 1, default);
+insert into oppdrag values (2, DATE('2020-05-12'), default, 2, 1, 2, NULl);
+insert into oppdrag values (3, DATE('2020-05-12'), DATE('2020-08-14'), 3, 2, 2, 9);
+insert into oppdrag values (4, DATE('2020-05-17'), DATE('2020-08-19'), 3, NULL, 2, 29);
+
+insert into kandidat_kvalifikasjon values(1, 1);
+insert into kandidat_kvalifikasjon values (2, 2);
+insert into kandidat_kvalifikasjon values (2, 3);
---
+++ Obliger/Oeving 6/Oppgave D.sql
@@ -0,0 +1,37 @@
+use ikhovind;
+#oppgave 1
+SELECT navn, telefon, epost from bedrift;
+#oppgave 2
+select oppdrag_id, org_nummer, navn, telefon
+from oppdrag
+left join bedrift b on b.orgnummer = oppdrag.org_nummer;
+#oppgave 3
+create view kand_kvalik_besk as
+    (select k.kvalifikasjon_id, kandidat_id, beskrivelse from kandidat_kvalifikasjon
+    left join kvalifikasjon k on kandidat_kvalifikasjon.kvalifikasjon_id = k.kvalifikasjon_id);
+
+select kandidat.kandidat_id, kandidat.fornavn, etternavn, kkb.beskrivelse, kvalifikasjon_id
+from kandidat
+right join kand_kvalik_besk kkb on kandidat.kandidat_id = kkb.kandidat_id;
+#oppgave 4
+select kandidat.kandidat_id, kandidat.fornavn, etternavn, kkb.beskrivelse, kvalifikasjon_id
+from kandidat
+left join kand_kvalik_besk kkb on kandidat.kandidat_id = kkb.kandidat_id;
+#oppgave 5
+create view ferdig_oppdrag as(
+select kandidat_id, sluttdato, oppdrag_id, org_nummer, beskrivelse from
+    (select *
+    from oppdrag
+    join
+        (select beskrivelse
+        from sluttatest_mal) as smb)
+    as malOpp
+where sluttdato IS NOT NULL);
+
+select fornavn, etternavn, sluttdato, oppdrag_id, beskrivelse, bedrift.navn from
+(select fornavn, etternavn, sluttdato, oppdrag_id, beskrivelse, org_nummer
+from ferdig_oppdrag
+left join kandidat k on ferdig_oppdrag.kandidat_id = k.kandidat_id
+where ferdig_oppdrag.kandidat_id = 2) as kand2_ferdig
+left join bedrift
+on bedrift.orgnummer = kand2_ferdig.org_nummer;
